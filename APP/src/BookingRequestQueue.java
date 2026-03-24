93cdc64f21ae920cd48a3c2d5fb5aa4102e72593
Jashmi10 <jashmirajij@gmail.com>
2026-03-24 16:12:04 +0530

    Adding request queue booking system

---
+++ APP/src/BookingRequestQueue.java
@@ -0,0 +1,28 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class BookingRequestQueue {
+
+    /** Queue that stores booking requests. */
+    private Queue<Reservation> requestQueue;
+
+    /** Initializes an empty booking queue. */
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    /** Adds a booking request to the queue. */
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    /** Retrieves and removes the next booking request from the queue. */
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
+
+    /** Checks whether there are pending booking requests. */
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
+    }
+}
